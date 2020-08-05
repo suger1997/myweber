805374097741d34ebe915bce9eb663f506a83ab2
suger <2516659315@qq.com>
2020-08-05 10:23:43 +0800

    启动

--- src/main/java/com/sy/myweber/MyweberApplication.java
+++ src/main/java/com/sy/myweber/MyweberApplication.java
@@ -4,9 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 @MapperScan("com.sy.myweber.mapper")
-
 public class MyweberApplication {
 
     public static void main(String[] args) {
